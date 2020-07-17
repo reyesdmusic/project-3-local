6e812d154994653612704687f66ced6bf75edf47
Danny Reyes <reyesdmusic@gmail.com>
2020-07-17 13:21:29 -0400

    latest merger

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,10 +1,11 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab, NavDropdown } from 'react-bootstrap';
 import UploadPhoto from '../UploadPhoto';
 import ProfilePic from '../ProfilePic';
 import "./style.css";
 
+import NotificationDropdownItem from '../NotificationDropdownItem/NotificationDropdownItem';
 import UserInfoContext from '../../utils/UserInfoContext';
 import AuthService from '../../utils/auth';
 
@@ -14,56 +15,62 @@ function AppNavbar() {
   // set modal display state
   const [showModal, setShowModal] = useState(false);
   // get username out of context object to display in nav
-  const { username, picture  } = useContext(UserInfoContext);
+  // const { username, picture, notifications  } = useContext(UserInfoContext);
+  const userData = useContext(UserInfoContext);
 
+  useEffect(() => {
+    userData.getUserData();
+  }, []);
+
+  // <NavDropdown.Item>{notification.likerUsername} liked your post of "{notification.title}"<Button>Oh, word!</Button></NavDropdown.Item>
 
   return (
     <>
       <Navbar sticky="top" expand='lg' id="new-navbar">
         <Container fluid>
-          {username ? <Navbar.Brand id="navbar-brand" as={Link} to='/home'>
+          {userData.username ? <Navbar.Brand id="navbar-brand" as={Link} to='/home'>
             <h5>WHAT'S GOOD</h5>
-          </Navbar.Brand> : 
-          <Navbar.Brand id="navbar-brand" as={Link} to='/'>
-            <h5>WHAT'S GOOD</h5>
-          </Navbar.Brand>}
+          </Navbar.Brand> :
+            <Navbar.Brand id="navbar-brand" as={Link} to='/'>
+              <h5>WHAT'S GOOD</h5>
+            </Navbar.Brand>}
 
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar-group'>
             <Nav className='ml-auto'>
 
-          
+
               {/* <Nav.Link className="nav-link-group" as={Link} to='/search-user'>
                 Search For Friends
               </Nav.Link> */}
-             
-              
-              
-              {username ?
-              
-              <NavDropdown className="nav-link-group" title="SEARCH" id="basic-nav-dropdown" >
-                <NavDropdown.Item href="/search-user">SEARCH FOR FRIENDS</NavDropdown.Item>
-                <NavDropdown.Item href="/search_books">SEARCH BOOKS</NavDropdown.Item>
-                <NavDropdown.Item href="/search_music">SEARCH MUSIC</NavDropdown.Item>
-                <NavDropdown.Item href="/search_movies">SEARCH MOVIES</NavDropdown.Item>
-                <NavDropdown.Item href="/search_games">SEARCH GAMES</NavDropdown.Item>
-              </NavDropdown>
 
-              :
 
-              <NavDropdown className="nav-link-group" title="SEARCH" id="basic-nav-dropdown" alignRight>
-                <NavDropdown.Item href="/search-user">SEARCH FOR FRIENDS</NavDropdown.Item>
-                <NavDropdown.Item href="/search_books">SEARCH BOOKS</NavDropdown.Item>
-                <NavDropdown.Item href="/search_music">SEARCH MUSIC</NavDropdown.Item>
-                <NavDropdown.Item href="/search_movies">SEARCH MOVIES</NavDropdown.Item>
-                <NavDropdown.Item href="/search_games">SEARCH GAMES</NavDropdown.Item>
-              </NavDropdown>
-              
-              
+
+              {userData.username ?
+
+                <NavDropdown className="nav-link-group" title="SEARCH" id="basic-nav-dropdown" >
+                  <NavDropdown.Item href="/search-user">SEARCH FOR FRIENDS</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_books">SEARCH BOOKS</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_music">SEARCH MUSIC</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_movies">SEARCH MOVIES</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_games">SEARCH GAMES</NavDropdown.Item>
+                </NavDropdown>
+
+                :
+
+                <NavDropdown className="nav-link-group" title="SEARCH" id="basic-nav-dropdown" alignRight>
+                  <NavDropdown.Item href="/search-user">SEARCH FOR FRIENDS</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_books">SEARCH BOOKS</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_music">SEARCH MUSIC</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_movies">SEARCH MOVIES</NavDropdown.Item>
+                  <NavDropdown.Item href="/search_games">SEARCH GAMES</NavDropdown.Item>
+                </NavDropdown>
+
+
               }
 
 
-              {username && <NavDropdown className="nav-link-group" title="MY MEDIA" id="basic-nav-dropdown">
+              {userData.username && <NavDropdown className="nav-link-group" title="MY MEDIA" id="basic-nav-dropdown">
                 <NavDropdown.Item href='/saved_media'>ALL MY MEDIA</NavDropdown.Item>
                 <NavDropdown.Item href='/saved_books'>MY BOOKS</NavDropdown.Item>
                 <NavDropdown.Item href="/saved_music">MY MUSIC</NavDropdown.Item>
@@ -71,19 +78,30 @@ function AppNavbar() {
                 <NavDropdown.Item href="/saved_games">MY GAMES</NavDropdown.Item>
               </NavDropdown>}
 
-              {username && <Nav.Link className="nav-link-group" as={Link} to='/saved-friends'>
+              {userData.username && <Nav.Link className="nav-link-group" as={Link} to='/saved-friends'>
                 MY FRIENDS
               </Nav.Link>}
-              
-              
-              {username && <NavDropdown className="btn dropdown-toggle" data-toggle="dropdown" alignRight title={
-                <ProfilePic 
-                  picture={picture}
-                  username={username}
-                />
-              } >
 
+
+              {userData.notifications.length > 0 && (<h4>{userData.notifications.length}</h4>)}
+
+              {userData.username && 
+                <NavDropdown alignRight title={
+                  <ProfilePic
+                    picture={userData.picture}
+                    username={userData.username}
+                  />
+                }>
                 <NavDropdown.Item onClick={() => setShowModal(true)}>UPLOAD PROFILE PIC</NavDropdown.Item>
+                {userData.notifications.map((notification) => {
+                  { console.log("notification in navbar", notification) }
+                  return (
+                    <NotificationDropdownItem
+                      likerUsername={notification.likerUsername}
+                      title={notification.title}
+                      notificationId={notification._id} />
+                  )
+                })}
 
                 <NavDropdown.Item onClick={AuthService.logout}>LOGOUT</NavDropdown.Item>
               </NavDropdown>}
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -10,6 +10,8 @@ import AuthService from '../../utils/auth';
 import * as API from '../../utils/API';
 import LikeButton from '../../components/LikeButton';
 
+import FeedCard from '../../components/FeedCard';
+
 
 function Home() {
 
@@ -20,14 +22,18 @@ function Home() {
   }
 
   const userData = useContext(UserInfoContext);
+  console.log("userDATA", userData);
+
+  // to pass into notifications so user knows who liked something
+  // const likerId = userData._id;
+  const likerUsername = userData.username;
 
   useEffect(() => {
 
     userData.friends.map(friend => {
-
       API.getUser(friend.id)
         .then(result => {
-  
+
           if (result.data.savedBooks.length > 0) {
 
             result.data.savedBooks.map(savedBook => {
@@ -71,6 +77,7 @@ function Home() {
                 _id: savedMusic._id,
                 username: friend.username,
                 picture: friend.picture,
+                userId: friend.id,
                 image: savedMusic.image,
                 title: savedMusic.title,
                 link: savedMusic.link,
@@ -103,6 +110,7 @@ function Home() {
                 _id: savedMovie._id,
                 username: friend.username,
                 picture: friend.picture,
+                userId: friend.id,
                 image: savedMovie.image,
                 title: savedMovie.title,
                 runtime: savedMovie.runtime,
@@ -136,36 +144,38 @@ function Home() {
                 _id: savedGame._id,
                 username: friend.username,
                 picture: friend.picture,
+                userId: friend.id,
                 image: savedGame.image,
                 title: savedGame.title,
                 developer: savedGame.developer,
                 description: savedGame.description,
                 likes: savedGame.likes
               }
-
               setAllFriendsMediaState(allFriendsMediaState => [...allFriendsMediaState, savedGameData].sort(compareTimeStamp))
-
-
             })
-
-
           }
 
-      })
 
-    });
+          // }
 
-}, [userData.username]);
+          // )
+
+        })
+
+        })
+    }, [userData.username]);
 
 
-  const handleSaveLike = useCallback((likeMediaType, like_id, mediaLikes) => {
- 
+  const handleSaveLike = useCallback((likeMediaType, like_id, mediaLikes, ownerId, title) => {
+    // find the friend in `searchedUser` state by the matching id
+    // const userToSave = searchedUser.find((user) => user._id === userId);
+
+    // get token
     const token = AuthService.loggedIn() ? AuthService.getToken() : null;
     if (!token) {
-        return false;
+      return false;
     }
 
-    
     let likeData = {
       mediaType: likeMediaType,
       mediaId: like_id,
@@ -177,184 +187,96 @@ function Home() {
       likes: mediaLikes
     }
 
+    // info for notification
+    const notficationData = {
+      likerUsername: likerUsername,
+      title: title,
+      ownerId: ownerId
+    }
+
     console.log("data for like, ", likeData)
     
     API.saveLike(likeData, token)
-        .then(() => {
-          console.log("Token: ", token, "likeData: ", likeData);
-            userData.getUserData();
-       
+      .then(() => {
+        console.log("Token: ", token, "likeData: ", likeData);
+        userData.getUserData();
 
-        })
-        .catch((err) => console.log(err));
+
+      })
+      .catch((err) => console.log(err));
 
     API.addLike(addLikeData, token)
-        .then(() => {
-         
-            userData.getUserData();
-       
+      .then(() => {
 
-        })
-        .catch((err) => console.log(err));     
+        userData.getUserData();
+
+
+      })
+      .catch((err) => console.log(err));
+    //call to send notification to user  
+    
+    API.addNotification(notficationData, token)
+      .then(() => {
+        console.log("NOTIFICATION ADDED");
+        userData.getUserData();
+      })
   });
 
 
   return (
-
-
-
     <>
-     
-
+      {/* <Jumbotron fluid className='text-light bg-dark'>
+        <Container>
+          <h1>Viewing friends Media!</h1>
+        </Container>
+      </Jumbotron> */}
       <Container >
        
         <Row className="justify-content-center">
           <Col xs={12} md={8} >
-
-
             {allFriendsMediaState.map(media => {
-
               if (media.mediaType === "book") {
-
-                  
                 return (
-
-                  <Card key={media._id} border='dark'>
-
-                    <Card.Body>
-                      {media.picture ? <Card.Img id="profile-pic" src={media.picture} alt={media.username} variant='top' /> : null}
-                      
-                      <Card.Text>{media.username}</Card.Text>
-                      <Card.Text>{moment(media.createdAt).calendar()}</Card.Text>
-                     
-                      {media.image ? <Card.Img id="media-pic" src={media.image} alt={`The cover for ${media.title}`} variant='top' /> : null}
-                      <Card.Title>{media.title}</Card.Title>
-                      <p className='small'>Authors: {media.authors}</p>
-                      <Card.Text>{media.description}</Card.Text>
-                      
-                     
-                      <LikeButton mediaLikes={media.likes} 
-                                  mediaType={media.mediaType}
-                                  mediaId={media._id}
-                                  cb={handleSaveLike}
-                                  userData={userData}
-                                  
-                                  
-                                  ></LikeButton>
-                      <Button className='btn-block btn-danger' >
-                        Comment
-                      </Button>
-                    </Card.Body>
-                  </Card>)
-
+                  <FeedCard
+                    mediaType='book'
+                    media={media}
+                    cb={handleSaveLike}
+                    userData={userData}
+                  />
+                );
               }
-
               if (media.mediaType === "music") {
-
                 return (
-
-
-                  <Card key={media._id} border='dark'>
-                    <Card.Body>
-                      {media.picture ? <Card.Img id="profile-pic"  src={media.picture} alt={media.username} variant='top' /> : null}
-
-                      <Card.Text>{media.username}</Card.Text>
-                      <Card.Text>{moment(media.createdAt).calendar()}</Card.Text>
-                      <Card.Img id="media-pic" src={media.image} alt={media.artist} variant='top' />
-                      <Card.Title>{media.title}</Card.Title>
-
-                      <p className='small'>Artist: {media.artist}</p>
-                    
-                      <ReactAudioPlayer id="music-player"
-                        src={media.preview}
-                        controls
-                      />
-                      <LikeButton mediaLikes={media.likes} 
-                                  mediaType={media.mediaType}
-                                  mediaId={media._id}
-                                  cb={handleSaveLike}
-                                  userData={userData}          
-                      ></LikeButton>
-                      <Button className='btn-block btn-danger' >
-                        Comment
-                      </Button>
-                    </Card.Body>
-                  </Card>)
-
+                  <FeedCard
+                    mediaType='music'
+                    media={media}
+                    cb={handleSaveLike}
+                    userData={userData}
+                  />
+                );
               }
-
               if (media.mediaType === "movie") {
-
                 return (
-
-
-                  <Card key={media._id} border='dark'>
-                    
-                    <Card.Body>
-
-                    {media.picture ? <Card.Img id="profile-pic"  src={media.picture} alt={media.username} variant='top' /> : null}
-                      <Card.Text>{media.username}</Card.Text>
-                      <Card.Text>{moment(media.createdAt).calendar()}</Card.Text>
-                      {media.image ? <Card.Img id="media-pic" src={media.image} alt={`The cover for ${media.title}`} variant='top' /> : null}
-                      <Card.Title>{media.title}</Card.Title>
-                      <p className='small'>Released: {media.released}</p>
-                      <p className='small'>Actors: {media.actors}</p>
-                      <p className='small'>Director: {media.director}</p>
-                      <p className='small'>Genre: {media.genre}</p>
-                      <p className='small'>Plot: {media.plot}</p>
-                      <p className='small'>Rated: {media.rated}</p>
-                      <p className='small'>Runtime: {media.runtime}</p>
-                      <LikeButton mediaLikes={media.likes} 
-                                  mediaType={media.mediaType}
-                                  mediaId={media._id}
-                                  cb={handleSaveLike}
-                                  userData={userData}          
-                      ></LikeButton>
-                      <Button className='btn-block btn-danger' >
-                        Comment
-                      </Button>
-                    </Card.Body>
-                  </Card>)
-
+                  <FeedCard
+                    mediaType='movie'
+                    media={media}
+                    cb={handleSaveLike}
+                    userData={userData}
+                  />
+                );
               }
 
               if (media.mediaType === "game") {
-
                 return (
-
-
-                  <Card key={media._id} border='dark'>
-                    
-                    <Card.Body>
-                    {media.picture ? <Card.Img  id="profile-pic" src={media.picture} alt={media.username} variant='top' /> : null}
-                      <Card.Text>{media.username}</Card.Text>
-                      <Card.Text>{moment(media.createdAt).calendar()}</Card.Text>
-                      {media.image ? <Card.Img id="media-pic" src={media.image} alt={`The image for ${media.title}`} variant='top' /> : null}
-                      <Card.Title>{media.title}</Card.Title>
-                      <p className='small'>Developer: {media.developer}</p>
-                      <Card.Text>{media.description}</Card.Text>
-                   
-
-                      <LikeButton mediaLikes={media.likes} 
-                                  mediaType={media.mediaType}
-                                  mediaId={media._id}
-                                  cb={handleSaveLike}
-                                  userData={userData}          
-                      ></LikeButton>
-
-                      <Button className='btn-block btn-danger' >
-                        Comment
-                      </Button>
-                    </Card.Body>
-                  </Card>)
+                  <FeedCard
+                    mediaType='game'
+                    media={media}
+                    cb={handleSaveLike}
+                    userData={userData}
+                  />
+                );
               }
-
-            }
-
-            )
-
-
-            }
+            })}
           </Col>
         </Row>
       </Container>
